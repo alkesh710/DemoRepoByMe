258335f8a78e93774c7d19979c4b50cde420703f
alkesh <alkesh@Alkesh>
2019-01-06 04:03:36 -0800

    Update TS_01

--- src/test/java/com/qa/test/LoginTest.java
+++ src/test/java/com/qa/test/LoginTest.java
@@ -3,10 +3,11 @@ package com.qa.test;
 import org.testng.annotations.Test;
 
 public class LoginTest {
-	@Test
+	@Test()
 public void login()
 {
 	System.out.println("login test");
+	System.out.println("Demo");
 }
 
 }
